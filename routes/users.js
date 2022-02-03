33a8b965700a3ad7283fa337fd24bf056c6abc99
Aaron Endsley <7246724+aaronendsley@users.noreply.github.com>
2022-02-02 20:04:26 -0600

    Scripts, and Route testing
    Added the scripts for nodemon, and then created a new route in the users route as part of a challenge

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,10 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  next();
 });
+router.get('/bob', function(req, res, next){
+  res.send('I Am Bob');
+} )
 
 module.exports = router;
